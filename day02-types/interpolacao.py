9512a64ee98f10cc9f65098354dac23ddf081685
Khaled <khalegjr@gmail.com>
2022-05-17 16:47:57 -0300

    usando interpolação de string

---
+++ day02-types/interpolacao.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+__version__ = "0.1.1"
+__author__ = "Júnior (Khaled)"
+
+email_tmpl = """
+Olá, %(nome)s
+
+Tem interesse em comprar %(produto)s?
+
+Este produto é ótimo para %(texto)s.
+
+Clique agora em %(link)s
+
+Apenas %(quantidade)d disponíveis!
+
+preço promocional %(preco).2f.
+"""

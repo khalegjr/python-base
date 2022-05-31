9b63d0c58b2c593c41eb181b32583e75cc19d79d
Khaled <khalegjr@gmail.com>
2022-05-31 14:44:04 -0300

    refatorando para utilizar dicionário

--- hello.py
+++ hello.py
@@ -16,7 +16,7 @@ Execução:
     ou
     ./hello.py
 """
-__version__ = "0.0.1"
+__version__ = "0.1.2"
 __author__ = "Júnior (Khaled)"
 __license__ = "Unlicense"
 
@@ -24,17 +24,14 @@ import os
 
 current_language = os.getenv("LANG", "en_US")[:5]
 
-if current_language == "pt_BR":
-    msg = "Olá, Mundo!"
-elif current_language == "it_IT":
-    msg = "Ciao, Mondo!"
-elif current_language == "es_SP":
-    msg = "Hola, Mundo!"
-elif current_language == "fr_FR":
-    msg = "Bonjour Monde!"
-elif current_language == "de_DE":
-    msg = "Hallo Welt!"
-else:
-    msg = "Hello, World!"
+# dicts tem Hash Tables - O(1)
+msg = {
+    "en_US": "Hello, World!",
+    "pt_BR": "Olá, Mundo!",
+    "it_IT": "Ciao, Mondo!",
+    "es_SP": "Hola, Mundo!",
+    "fr_FR": "Bonjour Monde!",
+    "de_DE": "Hallo Welt!"
+}
 
-print(msg)
+print(msg[current_language])

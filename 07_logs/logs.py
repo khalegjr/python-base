37f85e17b871a38fa74049dc48e39844d4a1b9e7
Khaled <khalegjr@gmail.com>
2022-08-01 04:16:31 -0300

    rotating file handler logs

--- 07_logs/logs.py
+++ 07_logs/logs.py
@@ -2,6 +2,7 @@
 
 import logging
 import os  # stderr root loggin
+from logging import handlers
 
 # BOILERPLATE
 # TODO: usar função
@@ -14,18 +15,26 @@ log_level = os.getenv("LOG_LEVEL", "WARNING").upper()
 log = logging.Logger("logs.py", log_level)
 
 # level
-ch = logging.StreamHandler()  # Console/terminal/stderr
-ch.setLevel(log_level)
+# ch = logging.StreamHandler()  # Console/terminal/stderr
+# ch.setLevel(log_level)
+fh = handlers.RotatingFileHandler(
+    "meulog.log",
+    maxBytes=500,  # o recomendado é 10**6 = 1Mb
+    backupCount=10,  # número de arquivos mantidos
+)
+fh.setLevel(log_level)
 
 # formatação
 fmt = logging.Formatter(
     '%(asctime)s %(name)s %(levelname)s '
     'l:%(lineno)d f:%(filename)s: %(message)s'
 )
-ch.setFormatter(fmt)
+# ch.setFormatter(fmt)
+fh.setFormatter(fmt)
 
 # destino
-log.addHandler(ch)
+# log.addHandler(ch)
+log.addHandler(fh)
 # Fim da configuração do log
 
 """
@@ -36,7 +45,7 @@ log.error("Erro que afeta uma única execução")
 log.critical("Erro geral ex: banco de dados sumiu")
 """
 
-print("---")
+# print("---")
 try:
     1/0
 except ZeroDivisionError as e:

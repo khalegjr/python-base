342369324d288a2f77d3440b2e41a550ca096a7a
Khaled <khalegjr@gmail.com>
2022-08-10 00:56:23 -0300

    primeira versão de números pares

---
+++ 09_exercicios/numeros_pares.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Faça um programa que imprime os números pares de 1 a 200
+ex
+`python3 numeros_pares.py`
+2
+4
+6
+8
+...
+"""
+__version__ = "0.1.0"
+__author__ = "Júnior (Khaled)"
+
+for number in range(0, 201, 2):
+    print(number)
